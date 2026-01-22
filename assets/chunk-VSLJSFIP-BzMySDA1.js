0abf5f5224bbd9840336847ac17d3ac776e33278
iiishop <30495867+iiishop@users.noreply.github.com>
2026-01-22 19:24:32 +0000

    Delete assets directory

--- assets/chunk-VSLJSFIP-BzMySDA1.js
+++
@@ -1,1 +0,0 @@
-import{ar as s}from"./MarkdownPanel-CpoSzjuQ.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as s};
